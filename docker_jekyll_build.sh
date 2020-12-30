8454fd03167bfc85a32b935bf2c8d054737185bf
Achim Abeling <achimabeling@web.de>
2020-12-30 15:24:25 +0100

    changed ci workflow to use another volume for the site

--- docker_jekyll_build.sh
+++ docker_jekyll_build.sh
@@ -3,4 +3,7 @@
 # see https://github.com/envygeeks/jekyll-docker/blob/master/README.md
 
 export JEKYLL_VERSION=4.2.0
-docker run --rm --volume="$PWD:/srv/jekyll" -it jekyll/minimal:$JEKYLL_VERSION jekyll build
+docker run --rm \
+  --volume="$PWD:/srv/jekyll" \
+  --volume="$PWD/_site:/srv/jekyll/_site" \
+  jekyll/minimal:$JEKYLL_VERSION jekyll build
