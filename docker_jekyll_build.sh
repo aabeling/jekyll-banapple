c70aa0aaaf13f91197308e71edf77ba11f536687
Achim Abeling <achimabeling@web.de>
2022-12-04 11:27:10 +0100

    Add post about mastodon
    Uses jekyll 3 now because 4 did not work anymore.
    Removed twitter links.

--- docker_jekyll_build.sh
+++ docker_jekyll_build.sh
@@ -2,8 +2,8 @@
 
 # see https://github.com/envygeeks/jekyll-docker/blob/master/README.md
 
-export JEKYLL_VERSION=4.2.0
+export JEKYLL_VERSION=3
 docker run --rm \
   --volume="$PWD:/srv/jekyll" \
   --volume="$PWD/_site:/srv/jekyll/_site" \
-  jekyll/minimal:$JEKYLL_VERSION jekyll build --config _config.yml,_config-github.yml
+  jekyll/builder:$JEKYLL_VERSION jekyll build --config _config.yml,_config-github.yml
--- docker_jekyll_serve.sh
+++ docker_jekyll_serve.sh
@@ -2,5 +2,6 @@
 
 # see https://github.com/envygeeks/jekyll-docker/blob/master/README.md
 
-export JEKYLL_VERSION=4.2.0
-docker run --rm --volume="$PWD:/srv/jekyll" -it -p 4000:4000 jekyll/minimal:$JEKYLL_VERSION jekyll serve --incremental --watch
+rm -rf _site
+export JEKYLL_VERSION=3
+docker run --rm --volume="$PWD:/srv/jekyll" -it -p 4000:4000 jekyll/jekyll:$JEKYLL_VERSION jekyll serve --incremental --watch --verbose
